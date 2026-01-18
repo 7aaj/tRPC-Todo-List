8e4b2949333e0dfa610a9020691d83660e0dd860
7aaj <mo7ammad_7aaj@hotmail.com>
2026-01-18 15:41:06 +0300

    Initial tRPC Todo List project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { serverClient } from "@/app/_trpc/serverClient";
+import TodoList from "./_components/TodoList";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const todos = await serverClient.getTodos();
+  return (
+    <main className="max-w-3xl mx-auto mt-5 ">
+      <TodoList initialTodos={todos} />
+    </main>
+  );
+}
